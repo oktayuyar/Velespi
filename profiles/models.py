56d6f8537c2b54b4433d130f45784f31ae4ac583
oktayuyar <oktyuyar@gmail.com>
2017-06-01 13:36:26 +0300

    rest api üye kaydı yapıldı

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User=get_user_model()
+class UserManager(models.User):
+
+    def validate(self, data):
+        username = data["username"]
+        user_gs = User.objects.filter(username=username)
+        if user_gs.exists():
+            raise ValidationError("Bu kullanıcı adı kullanılmaktadır.")
+        return data
--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 User=get_user_model()
 
@@ -7,10 +8,17 @@ class  UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("id", "username","password","email" )
-        extra_kwargs={
-            "password" :{
-                "write_only" :True}
-                }
+        #extra_kwargs={
+         #   "password" :{
+         #       "write_only" :True}
+         #       }
+
+    def validate(self, data):
+        username=data["username"]
+        user_gs=User.objects.filter(username=username)
+        if user_gs.exists():
+            raise ValidationError("Bu kullanıcı adı kullanılmaktadır.")
+        return data
 
     def create(self, validated_data):
         username=validated_data["username"]
