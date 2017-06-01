b0c42bb3fc488fc69f3598b4d4b6fed377cfd34c
oktayuyar <oktyuyar@gmail.com>
2017-06-01 21:27:26 +0300

    rest api üye kayıt,listeleme yapıldı

--- activities/serializers.py
+++ activities/serializers.py
@@ -1,14 +1,25 @@
-from activities.models import Activity,Review,Media,Category
 from rest_framework import serializers
-import activities
+from rest_framework.fields import SerializerMethodField
+from rest_framework.relations import StringRelatedField
+
+from activities import models
+from activities.models import Activity,Review,Media,Category
+
 
 class  ActivitySerializer(serializers.ModelSerializer):
-
+    user = SerializerMethodField()
+    category=SerializerMethodField()
     class Meta:
         model = Activity
         fields = ("id", "user", "name","route","description",
                   "is_active","category","telephone","likes")
 
+    def get_category(self,obj):
+        return str(obj.category.name)
+
+
+    def get_user(self, obj):
+        return str(obj.user.username)
 
 class  CategorySerializer(serializers.ModelSerializer):
     class Meta:
